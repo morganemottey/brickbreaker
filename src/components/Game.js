5b346a1d2a41a0d1e3803110bdee45af0b61f5c5
stéphane <gervaisstephane@orange.fr>
2019-10-23 17:49:21 +0200

    vertion bricks block cadrée + bill

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
 import './Game.css';
 import Point from './Point';
-import Line from './Line';
+import Bricks from './Bricks';
 
 class Game extends Component {
   constructor(props) {
     super(props)
     this.state = {
       pointLeft: 20,
-      pointTop:647,
+      pointTop:587,
       goRight:true,
       goDown:false,
       xSpeed:1,
-      ySpeed:1
+      ySpeed:1,
+      brickWall: this.getBrickWall()
     }
   }
 
@@ -27,13 +28,15 @@ BriqueY = (left, top) => {
 
 MoovingX = () => {
   if (this.state.goRight){
+    // eslint-disable-next-line 
       this.setState({pointLeft : this.state.pointLeft+=this.state.xSpeed})
   } else if (!this.state.goRight){
+    // eslint-disable-next-line 
       this.setState({pointLeft : this.state.pointLeft-=this.state.xSpeed})
   }
   
 
-  if (this.state.pointLeft > 355 || this.state.pointLeft < 0
+  if (this.state.pointLeft > 355 || this.state.pointLeft < 0 
     ){
       this.setState({goRight : !this.state.goRight})
   }
@@ -43,13 +46,15 @@ MoovingX = () => {
 
 MoovingY = () => {
   if (this.state.goDown){
+    // eslint-disable-next-line 
       this.setState({pointTop : this.state.pointTop+=this.state.ySpeed})
   } else if (!this.state.goDown){
+    // eslint-disable-next-line 
       this.setState({pointTop : this.state.pointTop-=this.state.ySpeed})
   }
   
 
-  if (this.state.pointTop > 647 || this.state.pointTop < 0
+  if (this.state.pointTop > 587 || this.state.pointTop < 0 
   ){
       this.setState({goDown : !this.state.goDown})
   }
@@ -62,13 +67,31 @@ componentDidMount(){
   this.MoovingY()
 }
 
+getBrickWall = () => {
+  const brick = [];
+      for (let i = 0; i < 6; i++) {     
+          for (let j = 0; j < 5; j++){
+             brick.push ({top:i*20, left:j*75})
+          } 
+      }
+  return brick;
+};
+
   render(){
     const {pointLeft, pointTop} = this.state
-
     return (
       <div className="Game">
-        <Line />
+        <div style={{position: 'relative', border: '1px red solid', height:'600px', width:'375', top:'67px'}}>
+          {this.state.brickWall.map( item => {
+            return(
+              <Bricks 
+              top={item.top}
+              left={item.left}
+              />
+            ); 
+          })}
         <Point left={pointLeft} top={pointTop}/>
+        </div>
       </div>
     );
   }
