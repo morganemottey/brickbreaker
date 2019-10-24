ebf601f116ea84101674268b180bb78b513f4647
Charles Gourdin <gourdin.charles@gmail.com>
2019-10-24 14:16:26 +0200

    Collision des briques OK, methodes renommées proprement. Reste Suppression de briques à l'impact.

--- src/components/Game.js
+++ src/components/Game.js
@@ -17,71 +17,71 @@ class Game extends Component {
     }
   }
 
-BriqueX = (left, top) => {
-  return (this.state.pointTop+19>top && this.state.pointTop<top+9 && this.state.pointLeft+20>left && this.state.pointLeft<left+65)
-}
-
-BriqueY = (left, top) => {
-  return (this.state.pointTop+20>top && this.state.pointTop<top+10 && this.state.pointLeft+19>left && this.state.pointLeft<left+64)
-}
-
-bricksPositionX = () => {
-  return (this.state.brickWall
-    .filter( item => {
-      return this.BriqueX(item.left, item.top)===true
-    })
-    .map(item=>{
-      return this.BriqueX(item.left, item.top)
-    }))
-    .join('||')
-}
-
-bricksPositionY = () => {
-  return (this.state.brickWall
-    .filter( item => {
-      return this.BriqueY(item.left, item.top)===true
-    })
-    .map( item => {
-      return this.BriqueY(item.left, item.top)
-    }))
-    .join('||')
-}
-
-MoovingX = () => {
-  if (this.state.goRight){
-    // eslint-disable-next-line 
-      this.setState({pointLeft : this.state.pointLeft+=this.state.xSpeed})
-  } else if (!this.state.goRight){
-    // eslint-disable-next-line 
-      this.setState({pointLeft : this.state.pointLeft-=this.state.xSpeed})
+  generateIfCollideX = (left, top) => {
+    return (this.state.pointTop+19>top && this.state.pointTop<top+9 && this.state.pointLeft+20>left && this.state.pointLeft<left+65)
   }
-  
 
-  if (this.state.pointLeft > 355 || this.state.pointLeft < 0 || this.bricksPositionX()
+  generateIfCollideY = (left, top) => {
+    return (this.state.pointTop+20>top && this.state.pointTop<top+10 && this.state.pointLeft+19>left && this.state.pointLeft<left+64)
+  }
+
+  checkIfCollideX = () => {
+    return (this.state.brickWall
+      .filter( item => {
+        return this.generateIfCollideX(item.left, item.top)===true
+      })
+      .map(item=>{
+        return this.generateIfCollideX(item.left, item.top)
+      }))
+      .join('||')
+  }
+
+  checkIfCollideY = () => {
+    return (this.state.brickWall
+      .filter( item => {
+        return this.generateIfCollideY(item.left, item.top)===true
+      })
+      .map( item => {
+        return this.generateIfCollideY(item.left, item.top)
+      }))
+      .join('||')
+  }
+
+  MoovingBallX = () => {
+    if (this.state.goRight){
+      // eslint-disable-next-line 
+        this.setState({pointLeft : this.state.pointLeft+=this.state.xSpeed})
+    } else if (!this.state.goRight){
+      // eslint-disable-next-line 
+        this.setState({pointLeft : this.state.pointLeft-=this.state.xSpeed})
+    }
+    
+
+    if (this.state.pointLeft > 355 || this.state.pointLeft < 0 || this.checkIfCollideX()
+      ){
+        this.setState({goRight : !this.state.goRight})
+    }
+
+    setTimeout(this.MoovingBallX, 1)
+  }
+
+  MoovingBallY = () => {
+    if (this.state.goDown){
+      // eslint-disable-next-line 
+        this.setState({pointTop : this.state.pointTop+=this.state.ySpeed})
+    } else if (!this.state.goDown){
+      // eslint-disable-next-line 
+        this.setState({pointTop : this.state.pointTop-=this.state.ySpeed})
+    }
+    
+
+    if (this.state.pointTop > 587 || this.state.pointTop < 0 || this.checkIfCollideY()
     ){
-      this.setState({goRight : !this.state.goRight})
+        this.setState({goDown : !this.state.goDown})
+    }
+
+    setTimeout(this.MoovingBallY, 1) 
   }
-
-  setTimeout(this.MoovingX, 1)
-}
-
-MoovingY = () => {
-  if (this.state.goDown){
-    // eslint-disable-next-line 
-      this.setState({pointTop : this.state.pointTop+=this.state.ySpeed})
-  } else if (!this.state.goDown){
-    // eslint-disable-next-line 
-      this.setState({pointTop : this.state.pointTop-=this.state.ySpeed})
-  }
-  
-
-  if (this.state.pointTop > 587 || this.state.pointTop < 0 || this.bricksPositionY()
-  ){
-      this.setState({goDown : !this.state.goDown})
-  }
-
-  setTimeout(this.MoovingY, 1) 
-}
 
   getBrickWall = () => {
     const brick = [];
@@ -94,8 +94,8 @@ MoovingY = () => {
   };
 
   componentDidMount(){
-    this.MoovingX()
-    this.MoovingY()
+    this.MoovingBallX()
+    this.MoovingBallY()
   }
 
   render(){
