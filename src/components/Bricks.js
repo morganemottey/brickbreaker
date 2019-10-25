920c760817d26f43485249e8c2fb4274abcbd69e
Maïté Arm <armengaud.m@gmail.com>
2019-10-25 10:31:57 +0200

    version stable pour présentation du 25/10/19

--- src/components/Bricks.js
+++ src/components/Bricks.js
@@ -9,9 +9,9 @@ class Bricks extends Component {
     render() { 
       const {left, top} = this.props
       const brickSize = {
-         width:`65px`,
-         height:`10px`,
-         backgroundColor:'Tan',
+         width:'67px',
+         height:'15px',
+         backgroundColor:'#75cac3',
          position:'absolute',
       }  
       return (
