a71c644bda489182f125ce37400dfd227677e24b
Charles Gourdin <gourdin.charles@gmail.com>
2019-10-29 11:58:03 +0100

    Ajout de bonus qui tombent à chaque suppression de briques. next step aléatoire.

---
+++ src/components/Bonus.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import donuts from './images/Donuts.png'
+
+class Bonus extends Component {
+    constructor(props){
+        super(props)
+        this.style={
+            width:'50px',
+            position:'absolute'
+        }
+        this.state={
+            top:this.props.top,
+            display:'block'
+        }
+    }
+
+    falling=()=>{
+        if(this.state.top<587){
+            this.setState({top: this.state.top+3})
+        }else{
+            this.setState({display: 'none'})
+        }
+        setTimeout(this.falling, 20)
+    }
+
+    componentDidMount=()=>{
+        this.falling()
+    }
+
+    render(){
+        const {left}=this.props
+        const {top, display}=this.state
+        return(
+            <>
+                <img style={{...this.style, top:top , left:left, display:display}} src={donuts} alt="Donuts" />
+            </>
+        )
+    }
+}
+
+export default Bonus
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,22 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Game.css';
 import Point from './Point';
 import Bricks from './Bricks';
 import Pad from './Pad';
+import Bonus from './Bonus';
 
 class Game extends Component {
   constructor(props) {
     super(props)
-    this.goRight=true;
-    this.goDown=false;
+    this.goRight = true;
+    this.goDown = false;
     this.state = {
       pointLeft: 20,
-      pointTop:587,
-      brickWall: this.getBrickWall()
+      pointTop: 587,
+      brickWall: this.getBrickWall(),
+      bonus: []
     }
   }
 
-  deleteBricks=()=>{
+  deleteBricks = () => {
     this.setState({brickWall:this.state.brickWall
       .filter( item => {
         return !(this.state.pointTop+20>item.top && this.state.pointTop<item.top+10 && this.state.pointLeft+20>item.left && this.state.pointLeft<item.left+65)
@@ -26,97 +28,122 @@ class Game extends Component {
       })})
   }
 
+  getBonus = () => {
+    const newDonutsTab = this.state.brickWall
+      .filter( item => {
+        return (this.state.pointTop+20>item.top && this.state.pointTop<item.top+10 && this.state.pointLeft+20>item.left && this.state.pointLeft<item.left+65)
+      })
+      .map( item => {
+        return item
+      })
+      newDonutsTab.push(...this.state.bonus)
+      this.setState({bonus: newDonutsTab})
+  }
+
   generateIfCollideX = (left, top) => {
-    return (this.state.pointTop+19>top && this.state.pointTop<top+9 && this.state.pointLeft+20>left && this.state.pointLeft<left+65)
+    return (this.state.pointTop + 19 > top && this.state.pointTop < top + 9 && this.state.pointLeft + 20 > left && this.state.pointLeft < left + 65)
   }
 
   generateIfCollideY = (left, top) => {
-    return (this.state.pointTop+20>top && this.state.pointTop<top+10 && this.state.pointLeft+19>left && this.state.pointLeft<left+64)
+    return (this.state.pointTop + 20 > top && this.state.pointTop < top + 10 && this.state.pointLeft + 19 > left && this.state.pointLeft < left + 64)
   }
 
   checkIfCollideX = () => {
     return (this.state.brickWall
-      .find( item => {
-        return this.generateIfCollideX(item.left, item.top)===true
+      .find(item => {
+        return this.generateIfCollideX(item.left, item.top) === true
       }))
   }
 
   checkIfCollideY = () => {
     return (this.state.brickWall
-      .find( item => {
-        return this.generateIfCollideY(item.left, item.top)===true
+      .find(item => {
+        return this.generateIfCollideY(item.left, item.top) === true
       }))
   }
 
   MoovingBallX = () => {
-    if (this.goRight){
+    if (this.goRight) {
       // eslint-disable-next-line 
-        this.setState({pointLeft : this.state.pointLeft+=1})
-    } else if (!this.goRight){
+      this.setState({ pointLeft: this.state.pointLeft += 1 })
+    } else if (!this.goRight) {
       // eslint-disable-next-line 
-        this.setState({pointLeft : this.state.pointLeft-=1})
+      this.setState({ pointLeft: this.state.pointLeft -= 1 })
     }
-    
 
-    if (this.state.pointLeft > 355 || this.state.pointLeft < 0 || this.checkIfCollideX()
-      ){
-        this.goRight=!this.goRight
-        this.deleteBricks()
+
+    if (this.state.pointLeft > 355 || this.state.pointLeft < 0) {
+      this.goRight = !this.goRight
+    } else if (this.checkIfCollideX()) {
+      this.goRight = !this.goRight
+      this.getBonus()
+      this.deleteBricks()
     }
 
     setTimeout(this.MoovingBallX, 1)
   }
 
   MoovingBallY = () => {
-    if (this.goDown){
+    if (this.goDown) {
       // eslint-disable-next-line 
-        this.setState({pointTop : this.state.pointTop+=1})
-    } else if (!this.goDown){
+      this.setState({ pointTop: this.state.pointTop += 1 })
+    } else if (!this.goDown) {
       // eslint-disable-next-line 
-        this.setState({pointTop : this.state.pointTop-=1})
+      this.setState({ pointTop: this.state.pointTop -= 1 })
     }
-    
 
-    if (this.state.pointTop > 587 || this.state.pointTop < 0 || this.checkIfCollideY()
-    ){
-        this.goDown=!this.goDown
+
+    if (this.state.pointTop > 587 || this.state.pointTop < 0) {
+      this.goDown = !this.goDown
+    } else if (this.checkIfCollideY()) {
+        this.goDown = !this.goDown
+        this.getBonus()
         this.deleteBricks()
     }
 
-    setTimeout(this.MoovingBallY, 1) 
+    setTimeout(this.MoovingBallY, 1)
   }
 
   getBrickWall = () => {
     const brick = [];
-        for (let i = 0; i < 6; i++) {     
-            for (let j = 0; j < 5; j++){
-              brick.push ({top:i*25, left:j*77})
-            } 
-        }
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 5; j++) {
+        brick.push({ top: i * 25, left: j * 77 })
+      }
+    }
     return brick;
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.MoovingBallX()
     this.MoovingBallY()
   }
 
-  render(){
-    const {pointLeft, pointTop} = this.state
+  render() {
+    const { pointLeft, pointTop } = this.state
     return (
       <div className="Game">
-        <div style={{position: 'relative', height:'600px', width:'375', top:'67px'}}>
-          {this.state.brickWall.map( item => {
-            return(
-              <Bricks 
-              top={item.top}
-              left={item.left}
-              key={item.top+'-'+item.left}
+        <div style={{ position: 'relative', height: '600px', width: '375', top: '67px' }}>
+          {this.state.brickWall.map(item => {
+            return (
+              <Bricks
+                top={item.top}
+                left={item.left}
+                key={item.top + '-' + item.left}
               />
-            ); 
+            );
           })}
-        <Point left={pointLeft} top={pointTop}/>
-        <Pad/>
+          {this.state.bonus.map(item => {
+            return (
+              <Bonus
+                top={item.top}
+                left={item.left+10}
+                key={item.top + '-' + item.left}
+              />
+            );
+          })}
+          <Point left={pointLeft} top={pointTop} />
+          <Pad />
         </div>
       </div>
     );
