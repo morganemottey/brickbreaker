a8beed197b542db0900124ceee9c54078151e75e
stéphane <gervaisstephane@orange.fr>
2019-11-05 13:51:52 +0100

    popup plus base css

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react'
 import './Homepage.css';
 import Parameters from '../components/Parameters';
-import {
-   Link
-  } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 class Homepage extends Component {
  
-
     render() {
         return (
-            <div className="title">
+            <div >
                 <Parameters/>
-                <h1>Homer's Nervous Brickdown</h1>
+                <h1 className="titleHome">Homer's Nervous Brickdown</h1>
                 <img src={require('../images/bart_fall.png')} alt="bart" />
-                 <button> <Link to ="/Game">Play</Link></button>
+                <button><Link to ="/Game">Play</Link></button>
             </div>
         );
     }
