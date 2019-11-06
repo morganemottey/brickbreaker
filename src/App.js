0e07f2086164b38e190148da0705ec8dde5663e0
Clara Nahornyj <c.nahornyj@hotmail.fr>
2019-11-06 10:39:32 +0100

    toto

--- src/App.js
+++ src/App.js
@@ -2,29 +2,11 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/Homepage';
 import Game from './pages/Game';
-<<<<<<< HEAD
 import { Route, BrowserRouter } from 'react-router-dom';
-import Popuploose from './components/Popuploose';
-import Popupwin from './components/Popupwin';
-=======
-import {
-  BrowserRouter,
-  Route,
-} from "react-router-dom";
->>>>>>> a8beed197b542db0900124ceee9c54078151e75e
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-    <BrowserRouter>
-     <div>
-       <Route path="/" exact component={Homepage}/>
-       <Route path="/Game" exact component={Game}/>
-     </div>
-    </BrowserRouter>
- </div>
-=======
        <BrowserRouter> 
         <div>
           <Route path="/" exact component={Homepage}/>
@@ -33,7 +15,6 @@ function App() {
        </BrowserRouter>  
     </div>
    
->>>>>>> a8beed197b542db0900124ceee9c54078151e75e
   );
 }
 
