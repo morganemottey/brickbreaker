86bd0b00d9e45b6cd376de95503acdf65ae065cf
Charles Gourdin <gourdin.charles@gmail.com>
2019-11-06 11:44:10 +0100

    Merge OK, suppression de messages d'erreurs dans parameters et homepage

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -54,7 +54,7 @@ class Game extends Component {
   }
 
   getBonus = () => {
-    if(Math.ceil(Math.random()*5)===5){
+    if(Math.ceil(Math.random()*6)===6){
     const newDonutsTab = this.state.brickWall
       .filter( item => {
         return (this.state.pointTop+20>item.top && this.state.pointTop<item.top+10 && this.state.pointLeft+20>item.left && this.state.pointLeft<item.left+67)
