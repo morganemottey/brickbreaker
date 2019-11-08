865829cf876268a3dfc545402fcd0ddb744b665d
Charles Gourdin <gourdin.charles@gmail.com>
2019-11-08 14:45:51 +0100

    commit avant merge

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -261,10 +261,3 @@ class Game extends Component {
 }
 
 export default Game;
-
-// checkIfCollidePadY = () => {
-//   return (this.state.pointTop > 472
-//     && this.state.pointTop <= 474
-//     && this.state.pointLeft + 10 > this.state.xLeft - 60
-//     && this.state.pointLeft - 10 < this.state.xLeft + 60)
-// }
