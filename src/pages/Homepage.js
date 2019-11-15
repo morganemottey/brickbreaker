70d6178cfb5f528a5b5e9aaff983b086b17e3bda
Clara Nahornyj <c.nahornyj@hotmail.fr>
2019-11-15 10:58:43 +0100

    Modification des boutons de la page d'accueil du jeu

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -11,9 +11,9 @@ class Homepage extends Component {
                 <Parameters />
                 <h1>Homer's Nervous Brickdown</h1>
                 <img src={require('../images/bart_fall.png')} alt="bart" />
-                <div>
-                    <button className="linktogame"> <Link to ="/Game">Play</Link></button>
-                    <button className="linktogame" onClick={this.props.onClick}> <Link to ="/Game">Time trial</Link></button>
+                <div className="buttons_homepage">
+                    <button className="linktogame"> <Link to ="/Game">LEVEL 1</Link></button>
+                    <button className="linktogame" onClick={this.props.onClick}> <Link to ="/Game">LEVEL 2</Link></button>
                 </div>
             </div>
         );
